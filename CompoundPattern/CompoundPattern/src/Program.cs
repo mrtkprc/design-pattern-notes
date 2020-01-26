9a6288a12d4cca5cd0bdc3ee89ec276aaf2c0fdc
mrtkprc <123456ORKebru>
2020-01-26 17:56:46 +0300

    Compount Pattern Initialization.

---
+++ CompoundPattern/CompoundPattern/src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CompoundPattern
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Compound Pattern Section In Head First Design Pattern");
+
+        }
+    }
+}
